d4274d8b779a21f2cb53efabe4e6e57367a25c62
eugenecha <detergentyoon@gmail.com>
2023-12-12 21:51:04 +0900

    사용자 입력을 통한 동적인 username 조회
    # 입력 창에 username 입력 후 GET 버튼으로 사용자 조회

--- app/src/main/java/kr/hxi/retrofit_example/MainActivity.java
+++ app/src/main/java/kr/hxi/retrofit_example/MainActivity.java
@@ -1,13 +1,16 @@
 package kr.hxi.retrofit_example;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -23,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private RetrofitService retrofitService;
 
+    private EditText etUsername;
+    private AppCompatButton btnGet;
+
     private RecyclerView rvUserList;
     private UserAdapter rvUserListAdapter;
 
@@ -33,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etUsername = findViewById(R.id.et_username);
+        btnGet = findViewById(R.id.btn_get);
         rvUserList = findViewById(R.id.rv_user_list);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
@@ -41,37 +49,39 @@ public class MainActivity extends AppCompatActivity {
         // Retrofit 인터페이스 구현체 가져오기
         retrofitService = RetrofitClient.getRetrofitService();
 
-        String username = "detergentyoon";
+        btnGet.setOnClickListener(view -> {
+            String username = etUsername.getText().toString();
+            Call<UserModel> call = retrofitService.getUserData(username);
+            call.enqueue(new Callback<UserModel>() {
+                @Override
+                public void onResponse(Call<UserModel> call, Response<UserModel> response) {
+                    if (response.isSuccessful()) {
+                        UserModel user = response.body();
+                        Log.d("Retrofit Status", user.toString());
 
-        Call<UserModel> call = retrofitService.getUserData(username);
-        call.enqueue(new Callback<UserModel>() {
-            @Override
-            public void onResponse(Call<UserModel> call, Response<UserModel> response) {
-                if (response.isSuccessful()) {
-                    UserModel user = response.body();
-                    Log.d("Retrofit Status", user.toString());
+                        String login = user.getLogin();
+                        int id = user.getId();
+                        String name = user.getName();
+                        String htmlUrl = user.getHtmlUrl();
+                        String location = user.getLocation();
 
-                    String login = user.getLogin();
-                    int id = user.getId();
-                    String name = user.getName();
-                    String htmlUrl = user.getHtmlUrl();
-                    String location = user.getLocation();
+                        user = new UserModel(login, id, name, htmlUrl, location);
 
-                    user = new UserModel(login, id, name, htmlUrl, location);
+                        rvUserListAdapter = new UserAdapter(userList);
+                        rvUserList.setAdapter(rvUserListAdapter);
 
-                    rvUserListAdapter = new UserAdapter(userList);
-                    rvUserList.setAdapter(rvUserListAdapter);
-
-                    userList.add(user);
-                } else {
-                    Log.e("Retrofit Status", "요청 실패");
+                        userList.add(user);
+                    } else {
+                        Toast.makeText(MainActivity.this, R.string.non_existent_user, Toast.LENGTH_SHORT).show();
+                        Log.e("Retrofit Status", "요청 실패");
+                    }
                 }
-            }
 
-            @Override
-            public void onFailure(Call<UserModel> call, Throwable t) {
-                Log.e("Retrofit Status", "네트워크 오류 또는 요청 실패: " + t.getMessage());
-            }
+                @Override
+                public void onFailure(Call<UserModel> call, Throwable t) {
+                    Log.e("Retrofit Status", "네트워크 오류 또는 요청 실패: " + t.getMessage());
+                }
+            });
         });
     } // onCreate
 }
